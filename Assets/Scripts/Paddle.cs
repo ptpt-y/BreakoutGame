5b0f728e9e40ef5698be0df9d202c243ec4b42eb
Tingyu <tingyupeng@126.com>
2023-07-02 19:56:54 +0800

    add paddle and ball movement

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    #region Singleton
+    private static Paddle _instance;
+    public static Paddle Instance => _instance;
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+    #endregion
+
+    private Camera mainCamera;
+    private float paddleInitialY;
+    private float defaultPaddleWidthInPixels = 200;
+    private float defautLeftClamp = 110;
+    private float defautRightClamp = 1810;
+    private SpriteRenderer sr;
+
+
+    private void Start()
+    {
+        mainCamera = FindObjectOfType<Camera>();
+        paddleInitialY = this.transform.position.y;
+        this.sr = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        PaddleMovement();
+    }
+
+    private void PaddleMovement()
+    {
+        float paddleShift = (defaultPaddleWidthInPixels - ((defaultPaddleWidthInPixels / 2) * this.sr.size.x)) / 2;
+        float leftClamp = defautLeftClamp - paddleShift;
+        float rightClamp = defautRightClamp + paddleShift;
+        float mousePositionPixels = Mathf.Clamp(Input.mousePosition.x, leftClamp, rightClamp);
+        float mousePositionWorldX = mainCamera.ScreenToWorldPoint(new Vector3(mousePositionPixels, 0, 0)).x;
+        this.transform.position = new Vector3(mousePositionWorldX, paddleInitialY, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Ball")
+        {
+            Rigidbody2D ballRb = coll.gameObject.GetComponent<Rigidbody2D>();
+            Vector3 hitPoint = coll.contacts[0].point;
+            Vector3 paddleCenter = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y);
+
+            ballRb.velocity = Vector2.zero;
+
+            float difference = paddleCenter.x - hitPoint.x;
+            if (hitPoint.x < paddleCenter.x)
+            {
+                ballRb.AddForce(new Vector2(-(Mathf.Abs(difference * 200)), BallsManager.Instance.initialBallSpeed));
+            }
+            else
+            {
+                ballRb.AddForce(new Vector2((Mathf.Abs(difference * 200)), BallsManager.Instance.initialBallSpeed));
+            }
+        }
+    }
+}
